233b1339d10121be47e9187e39c94d1f3d3a6154
arynch24 <arynch24@gmail.com>
2024-03-30 16:23:35 +0530

    sorting an array in increasing order

---
+++ Sorting/selection_sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //sorting in increasing order
+    
+    int arr[1000];
+    int n;
+    cout<<"Enter length of an array:";
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>arr[i]; 
+
+    for(int i=0;i<n-1;i++)
+    {
+        int index=i;   
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[index]>arr[j])
+            {
+              index=j;
+            }
+        }
+        swap(arr[index],arr[i]);
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
