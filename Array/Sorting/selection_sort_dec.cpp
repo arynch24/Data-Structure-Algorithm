dd5dc829316e9014dfa0be5b4a94cd19dde6eb81
arynch24 <arynch24@gmail.com>
2024-03-31 08:47:31 +0530

    selection sorting in array

---
+++ Array/Sorting/selection_sort_dec.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter length of an array:";
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    //this is in decreasing order but sorting is done from backword
+    for(int i=n-1;i>0;i--)
+    {
+        int index=i;
+        for (int j=i-1;j>=0;j--)
+        {
+            if(arr[i]>arr[j])
+            {
+                index=j;
+            }
+        }
+        swap(arr[index],arr[i]);
+    } 
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
