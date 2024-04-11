a8268558bc9fccb7534840bb8a8ca9fac09c86cc
arynch24 <arynch24@gmail.com>
2024-04-11 21:03:42 +0530

    highest element index

--- Array/Searching/Peak_Index.cpp
+++ Array/Searching/Peak_Index.cpp
@@ -1,3 +1,5 @@
+//leetcode 852
+
 //binary search can be used not only in increasing array 
 //bu can also in inc than dec
 // dec than inc
@@ -13,8 +15,8 @@ int peak_index(int arr[],int n)
     int start =0,end=n-1,mid;
     while(start<=end)
     {
-        
-        mid=start+(end-start)/2;
+        //runtime error solve 
+        mid=start+(start-end)/2;
         //peak element
         if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1] )
         return mid;
