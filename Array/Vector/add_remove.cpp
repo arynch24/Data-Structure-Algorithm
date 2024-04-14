2bc96b2a72d7b39dcb143373ad6482ff75b1767b
arynch24 <arynch24@gmail.com>
2024-04-15 01:19:43 +0530

    all vector uses

--- Array/Vector/add_remove.cpp
+++ Array/Vector/add_remove.cpp
@@ -7,16 +7,22 @@ int main()
 
     //ex
     vector<int> vec;
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity()<<endl;
     vec.push_back(1);//1 is stored  in array with [size=1, capacity=1] 
     vec.push_back(2);//1,2 is stored  in a new array with [size=2, capacity=2] and old one is deleted
     vec.push_back(3);//1,2,3 is stored  in a new array with [size=3, capacity=4] and old one is deleted
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity()<<endl;
     vec.push_back(4);//1,2,3,4 is stored  in a same array with [size=4, capacity=4] because there is already space.
+
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity()<<endl;
 
     //conclusin
     //vector doubles it memory/capacity whenever there is no space for an integer to store
     //due to which its average time complexity is always O(1).
     
-    cout<<vec.size()<<endl;
     for(int i=0;i<vec.size();i++)
     cout<<vec[i]<<" ";
     cout<<endl;
@@ -25,14 +31,20 @@ int main()
     vector<int>v={1,2,3,4,5,6,7,8,9};
     //how to remove value from vector.
     //there will be no change in capacity it will be same.
-    //v.pop_back;//to delete from last
+    v.pop_back();//to delete from last
     v.erase(v.begin()+2);//to remove of a particular place
     v.clear();//to remove all vslues
     //index 2 integer will be deleted and right elements will be shifted. 
 
     //front, back ,empty
     cout<<vec.front()<<" ";//to print first element
-    cout<<vec.back()<<" ";//to print last element
-    cout<<vec.empty()<<" ";//to tell that array is empty or not in bool datatype 0 and 1.
+    cout<<vec.back()<<"\n";//to print last element
+    cout<<vec.empty()<<"\n";//to tell that array is empty or not in bool datatype 0 and 1.
+
+    //how to insert an element at any position
+    vec.insert(vec.begin()+1,40);//inserting at first position
+    for(int i=0;i<vec.size();i++)
+    cout<<vec[i]<<" ";
+
 
 }
